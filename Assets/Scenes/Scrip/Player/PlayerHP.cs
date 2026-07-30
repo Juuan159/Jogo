59acbbdcb5de4689408b5acb5e079241db38fb9e
Juuan159 <0001228451@senaimgaluno.com.br>
2026-07-30 17:19:40 -0300

    Sistema de Vida

--- Assets/Scenes/Scrip/Player/PlayerHP.cs
+++ Assets/Scenes/Scrip/Player/PlayerHP.cs
@@ -1,21 +1,25 @@
+using TMPro;
 using UnityEngine;
 [RequireComponent(typeof(Player))]
 public class PlayerHP : MonoBehaviour
 {
     private Player player;
-    
+    [SerializeField] TMP_Text healthText;
     [SerializeField] int maxtHelt;
+    [SerializeField] Animator healthTextAnimator;
+
     void Start()
     {
         player = GetComponent<Player>();
         maxtHelt = player._health;
-        //healttex.text = "HP: "+ player._health +" / "+maxtHelt;
+        healthText.text = "HP: "+ player._health +" / "+maxtHelt;
     }
     
     public void ChanngeHealth(int amount)
     {
         player._health +=amount;
-        //healttex.text = "HP: "+ player._health +" / "+maxtHelt;
+        healthText.text = "HP: "+ player._health +" / "+maxtHelt;
+        healthTextAnimator.Play("HP_Text Animation");
         if(player._health <= 0)
         {
             player.gameObject.SetActive(false);
