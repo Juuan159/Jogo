6bde300e695fecd7fa4ca4a522311c22ef71b6c1
Juuan159 <0001228451@senaimgaluno.com.br>
2026-08-03 17:19:29 -0300

    Inimigo Mov

--- Assets/Scenes/Scrip/Inimigo/Inimigo.cs
+++ Assets/Scenes/Scrip/Inimigo/Inimigo.cs
@@ -5,19 +5,22 @@ public class Inimigo : MonoBehaviour
 {
     [SerializeField] private int damage = -10;
 
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log($"Colidiu com: {collision.gameObject.name}");
-        collision.gameObject.GetComponent<PlayerHP>().ChanngeHealth(damage);
-    }
-    void Start()
-    {
-        
-    }
 
-    void Update()
-    {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHP playerHP = collision.gameObject.GetComponent<PlayerHP>();
+
+            if (playerHP != null)
+            {
+                playerHP.ChanngeHealth(damage);
+            }
+            else
+            {
+                Debug.LogWarning($"Colidiu com o Player, mas o script 'PlayerHP' não foi encontrado no objeto!");
+            }
+        }
     }
 }
