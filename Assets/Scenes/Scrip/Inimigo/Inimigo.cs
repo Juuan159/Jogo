19b1c4aec11922d54490657abc7e4e2f51ca03c8
Juuan159 <0001228451@senaimgaluno.com.br>
2026-08-11 16:54:03 -0300

    KnockBack

--- Assets/Scenes/Scrip/Inimigo/Inimigo.cs
+++ Assets/Scenes/Scrip/Inimigo/Inimigo.cs
@@ -4,7 +4,9 @@ public class Inimigo : MonoBehaviour
 {
     [Header("Configurações do Ataque")]
     public int damage = 10; 
-    public float weaponRange = 1f; 
+    public float weaponRange = 1f;
+    public float knockbackForce;
+    public float stunTime;
     public Transform attackPoint; 
     public LayerMask playerLayer;
 
@@ -15,9 +17,11 @@ public class Inimigo : MonoBehaviour
         if (hits.Length > 0)
         {
             PlayerHP playerHealth = hits[0].GetComponent<PlayerHP>();
+            Player playerKnock= hits[0].GetComponent<Player>();
             if (playerHealth != null)
             {
                 playerHealth.ChanngeHealth(-damage);
+                playerKnock.Knockback(transform, knockbackForce, stunTime);
             }
         }
     }
--- Assets/Scenes/Scrip/Player/Player.cs
+++ Assets/Scenes/Scrip/Player/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +8,7 @@ public class Player : MonoBehaviour
     public float speed = 5f;
     private Vector2 direction;
     private Rigidbody2D rig; 
+    private bool isKnockedBack;
     
 
     public Vector2 _direction
@@ -47,7 +49,24 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if(isKnockedBack == false)
         rig.linearVelocity =Mover(direction) * (speed);           
+    }
+
+    public void Knockback(Transform Inimigo, float force, float stunTime)
+    {
+        isKnockedBack = true;
+        Vector2 direction = (transform.position - Inimigo.position).normalized;
+        rig.linearVelocity = direction * force;
+        if(_health > 0)
+        StartCoroutine(KnockbackCounter(stunTime));
+    }
+
+    IEnumerator KnockbackCounter(float stun)
+    {
+        yield return new WaitForSeconds(stun);
+        rig.linearVelocity = Vector2.zero;
+        isKnockedBack = false;
     }
 
     public int _health
